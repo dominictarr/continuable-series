f94d3cfb235bdbe245abdc67bf51a01392c79484
Dominic Tarr <dominic.tarr@gmail.com>
2013-06-05 20:55:07 +0200

    args->array

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-module.exports = function (continuables) {
+module.exports = function series (continuables) {
+  if('function' === typeof continuables)
+    return series([].slice.call(arguments))
   return function next (callback) {
     continuables.shift() (function (err, value) {
       if (err || !continuables.length)
